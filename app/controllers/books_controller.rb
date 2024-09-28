29ad9373082a98b7536b14287fe15f4cff6b54a0
tomoka <tomoka.saiwaki@gmail.com>
2024-09-28 10:47:50 +0900

    update: Update ym file configration
    ymlファイルの構成変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,7 +25,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to book_url(@book), notice: t('activerecord.common.message.success_create') }
+        format.html { redirect_to book_url(@book), notice: t('message.success_create') }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to book_url(@book), notice: t('activerecord.common.message.success_update') }
+        format.html { redirect_to book_url(@book), notice: t('message.success_update') }
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class BooksController < ApplicationController
     @book.destroy
 
     respond_to do |format|
-      format.html { redirect_to books_url, notice: t('activerecord.common.message.success_destroy') }
+      format.html { redirect_to books_url, notice: t('message.success_destroy') }
       format.json { head :no_content }
     end
   end
