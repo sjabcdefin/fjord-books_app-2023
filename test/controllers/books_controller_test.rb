99085dcc2370128cf4303368c5fcd44b4e405a37
tomoka <tomoka.saiwaki@gmail.com>
2024-09-28 10:04:22 +0900

    update: Add Japanese and English localization to tests
    ロケールが日本語・英語のどちらの場合でも、テストを実施できるよう修正

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -7,44 +7,85 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
     @book = books(:one)
   end
 
-  test 'should get index' do
-    get books_url
+  test 'should get index in Japanese' do
+    get books_url(locale: :ja)
     assert_response :success
   end
 
-  test 'should get new' do
-    get new_book_url
+  test 'should get index in English' do
+    get books_url(locale: :en)
     assert_response :success
   end
 
-  test 'should create book' do
+  test 'should get new in Japanese' do
+    get new_book_url(locale: :ja)
+    assert_response :success
+  end
+
+  test 'should get new in English' do
+    get new_book_url(locale: :en)
+    assert_response :success
+  end
+
+  test 'should create book in Japanese' do
     assert_difference('Book.count') do
-      post books_url, params: { book: { memo: @book.memo, title: @book.title } }
+      post books_url(locale: :ja), params: { book: { memo: @book.memo, title: @book.title } }
     end
 
-    assert_redirected_to book_url(Book.last)
+    assert_redirected_to book_url(Book.last, locale: :ja)
   end
 
-  test 'should show book' do
-    get book_url(@book)
+  test 'should create book in English' do
+    assert_difference('Book.count') do
+      post books_url(locale: :en), params: { book: { memo: @book.memo, title: @book.title } }
+    end
+
+    assert_redirected_to book_url(Book.last, locale: :en)
+  end
+
+  test 'should show book in Japanese' do
+    get book_url(@book, locale: :ja)
     assert_response :success
   end
 
-  test 'should get edit' do
-    get edit_book_url(@book)
+  test 'should show book in English' do
+    get book_url(@book, locale: :en)
     assert_response :success
   end
 
-  test 'should update book' do
-    patch book_url(@book), params: { book: { memo: @book.memo, title: @book.title } }
-    assert_redirected_to book_url(@book)
+  test 'should get edit in Japanese' do
+    get edit_book_url(@book, locale: :ja)
+    assert_response :success
   end
 
-  test 'should destroy book' do
+  test 'should get edit in English' do
+    get edit_book_url(@book, locale: :en)
+    assert_response :success
+  end
+
+  test 'should update book in Japanese' do
+    patch book_url(@book, locale: :ja), params: { book: { memo: @book.memo, title: @book.title } }
+    assert_redirected_to book_url(@book, locale: :ja)
+  end
+
+  test 'should update book in English' do
+    patch book_url(@book, locale: :en), params: { book: { memo: @book.memo, title: @book.title } }
+    assert_redirected_to book_url(@book, locale: :en)
+  end
+
+  test 'should destroy book in Japanese' do
     assert_difference('Book.count', -1) do
-      delete book_url(@book)
+      delete book_url(@book, locale: :ja)
     end
 
-    assert_redirected_to books_url
+    assert_redirected_to books_url(locale: :ja)
+  end
+
+  test 'should destroy book in English' do
+    assert_difference('Book.count', -1) do
+      delete book_url(@book, locale: :en)
+    end
+
+    assert_redirected_to books_url(locale: :en)
   end
 end
--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -7,25 +7,56 @@ class BooksTest < ApplicationSystemTestCase
     @book = books(:one)
   end
 
-  test 'visiting the index' do
-    visit books_url
+  test 'visiting the index in Japanese' do
+    visit books_url(locale: :ja)
+    assert_selector 'h1', text: '本一覧'
+  end
+
+  test 'visiting the index in English' do
+    visit books_url(locale: :en)
     assert_selector 'h1', text: 'Books'
   end
 
-  test 'should create book' do
-    visit books_url
+  test 'should create book in Japanese' do
+    visit books_url(locale: :ja)
+    click_on '新規追加'
+
+    Capybara.default_max_wait_time = 5
+    fill_in 'メモ', with: @book.memo
+    fill_in 'タイトル', with: @book.title
+    click_on '登録する'
+
+    assert_text '本が正常に作成されました。'
+    click_on '本一覧に戻る'
+  end
+
+  test 'should create book in English' do
+    visit books_url(locale: :en)
     click_on 'New book'
 
     fill_in 'Memo', with: @book.memo
     fill_in 'Title', with: @book.title
     click_on 'Create Book'
 
-    assert_text t('activerecord.common.message.success_create')
-    click_on 'Back'
+    assert_text 'Book was successfully created.'
+    click_on 'Back to books'
   end
 
-  test 'should update Book' do
-    visit book_url(@book)
+  test 'should update Book in Japanese' do
+    visit book_url(@book, locale: :ja)
+    click_on 'この本を編集する', match: :first
+
+    Capybara.default_max_wait_time = 5
+    fill_in 'メモ', with: @book.memo
+    fill_in 'タイトル', with: @book.title
+    click_on '更新する'
+
+    assert_text '本が正常に更新されました。'
+    click_on '本一覧に戻る'
+  end
+
+  test 'should update Book in English' do
+    visit book_url(@book, locale: :en)
     click_on 'Edit this book', match: :first
 
     fill_in 'Memo', with: @book.memo
@@ -33,11 +64,18 @@ class BooksTest < ApplicationSystemTestCase
     click_on 'Update Book'
 
     assert_text 'Book was successfully updated'
-    click_on 'Back'
+    click_on 'Back to books'
   end
 
-  test 'should destroy Book' do
-    visit book_url(@book)
+  test 'should destroy Book in Japanese' do
+    visit book_url(@book, locale: :ja)
+    click_on 'この本を削除する', match: :first
+
+    assert_text '本が正常に削除されました。'
+  end
+
+  test 'should destroy Book in English' do
+    visit book_url(@book, locale: :en)
     click_on 'Destroy this book', match: :first
 
     assert_text 'Book was successfully destroyed'
