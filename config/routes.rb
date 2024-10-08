b0ede22c16224bdbc3394d310ebbb33e8b1b66f3
tomoka <tomoka.saiwaki@gmail.com>
2024-10-08 22:17:19 +0900

    Create users controller and Add resources :users to routes.rb
    - usersコントローラの作成
    - routes.rbに、resources :usersを追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
   resources :books
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
