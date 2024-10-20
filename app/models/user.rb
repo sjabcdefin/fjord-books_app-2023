16a979f1545257d49fa91cf67e8ac9cf0fa229d3
tomoka <tomoka.saiwaki@gmail.com>
2024-10-20 14:01:52 +0900

    Update: Modify the indicated item of rubocop and review
    rubocopの指摘事項修正および見直し修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,18 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one_attached :avatar
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [300, 300]
+  end
 
   validate :verify_file_type
 
   private
 
   def verify_file_type
-    if avatar.attached?
-      allowed_file_types = %w[image/jpg image/jpeg image/gif image/png]
-      errors.add(:avatar, I18n.t('errors.messages.wrong_extention')) unless avatar.content_type.in?(allowed_file_types)
-    end
+    return unless avatar.attached?
+
+    allowed_file_types = %w[image/jpg image/jpeg image/gif image/png]
+    errors.add(:avatar, I18n.t('errors.messages.wrong_extention')) unless avatar.content_type.in?(allowed_file_types)
   end
 end
