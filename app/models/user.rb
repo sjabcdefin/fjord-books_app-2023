3bd4487db57e04471e3e701d5362627132b8eeb5
tomoka <tomoka.saiwaki@gmail.com>
2024-10-20 12:07:20 +0900

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :avatar
+
+  validate :verify_file_type
+
+  private
+
+  def verify_file_type
+    if avatar.attached?
+      allowed_file_types = %w[image/jpg image/jpeg image/gif image/png]
+      errors.add(:avatar, I18n.t('errors.messages.wrong_extention')) unless avatar.content_type.in?(allowed_file_types)
+    end
+  end
 end
