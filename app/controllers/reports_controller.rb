bf71e2ab800906e45fddf2308034db905cd38d5a
tomoka <tomoka.saiwaki@gmail.com>
2024-11-07 21:29:53 +0900

    Update: Correct the association of mentioning and mentioned
    言及先と言及元の関連付けを修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -9,7 +9,7 @@ class ReportsController < ApplicationController
 
   def show
     @report = Report.find(params[:id])
-    @mentioning_reports = @report.mentioning_reports
+    @mentioned_reports = @report.mentioned_reports
   end
 
   # GET /reports/new
