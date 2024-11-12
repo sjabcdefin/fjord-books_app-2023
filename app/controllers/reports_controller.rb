81069a2d344e358d6480023150ba20d17c5c0796
tomoka <tomoka.saiwaki@gmail.com>
2024-11-12 22:18:58 +0900

    Update: Modify so that mentions are also updated when the report is updated
    日報更新時に言及も更新されるよう修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -37,7 +37,16 @@ class ReportsController < ApplicationController
   end
 
   def update
-    if @report.update(report_params)
+    success = false
+
+    ActiveRecord::Base.transaction do
+      @report.update!(report_params)
+      @report.report_mentions.destroy_all
+      extract_and_save_urls(@report)
+      success = true
+    end
+
+    if success
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit, status: :unprocessable_entity
